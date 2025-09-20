b315ab00c7bb76c36b0cacdd5b2a0a8a866e9853
Arthur <arthur155188@gmail.com>
2025-09-20 21:36:13 +0500

    Создана модель для задач

--- todo/tasks/models.py
+++ todo/tasks/models.py
@@ -1,3 +1,32 @@
+import uuid
+from django.conf import settings
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Task(models.Model):
+    STATUS_CHOICES = (
+    ('todo', 'к исполнению'),
+    ('in_progress', 'в процессе'),
+    ('done', 'выполнено'),)
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='tasks')
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, default='')
+    status = models.CharField(choices=STATUS_CHOICES, max_length=20, default='todo')
+    priority = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], default=3)
+    due_date = models.DateField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['owner', 'status']),
+            models.Index(fields=['owner', 'priority']),
+            models.Index(fields=['due_date']),
+        ]
+        ordering = ['status', 'priority', 'due_date', '-created_at']
+
+    def __str__(self):
+        return f'{self.title} - ({self.get_status_display()})'
