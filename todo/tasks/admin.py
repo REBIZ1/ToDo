695ee9532a5ff0d3d88407d59a20b3078f7fb214
Arthur <arthur155188@gmail.com>
2025-09-20 21:36:53 +0500

    Модель Tasks зарегистрирована в админке

--- todo/tasks/admin.py
+++ todo/tasks/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'owner', 'title', 'status', 'priority', 'due_date')
+    list_filter = ('owner', 'status', 'priority')
